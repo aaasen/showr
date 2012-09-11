9806e237bc859260cf4541f8f7af59a742fb8533
Lane Aasen <laneaasen@gmail.com>
2012-09-11 09:53:44 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showr::Application.config.secret_token = 'a8dc6e582aa75742d5075d3253e9311c21bc5c3d6764c2bf1fb8db4cd75190ad6da6eb40119a153e074543481c317ab1bdfc80acb44c00e6675c2241d10f4271'
