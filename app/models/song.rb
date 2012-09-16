afb351ecca1c3db81ded9a0caf3fe9cc8a9f3859
Lane Aasen <laneaasen@gmail.com>
2012-09-15 20:36:10 -0700

    basic styling imported from shop.firedov.es

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,4 @@
 class Song < ActiveRecord::Base
   belongs_to :user, :foreign_key => "user_uid"
-  attr_accessible :url, :up_vote, :down_vote
+  attr_accessible :url, :up_vote, :down_vote, :name, :description
 end
